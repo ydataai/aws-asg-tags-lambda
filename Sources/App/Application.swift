efbd05061a0d0e447e7ce3461532b84dae81593a
Luís Portela Afonso <luis.portela@ydata.ai>
2022-06-21 20:13:23 +0100

    awsclient and register into terminator

--- Sources/App/Application.swift
+++ Sources/App/Application.swift
@@ -1,11 +1,19 @@
 import AWSLambdaRuntime
 import Models
+import SotoCore
+import SotoEKS
 
 public struct Application {
   let context: LambdaInitializationContext
 
+  let awsClient: AWSClient
+
   public init(context: LambdaInitializationContext) {
     self.context = context
+
+    self.awsClient = AWSClient(httpClientProvider: .createNewWithEventLoopGroup(self.context.eventLoop))
+
+    self.context.terminator.register(name: "\(type(of: AWSClient.self))", handler: self.awsClient.shutdown)
   }
 
   public func run(properties: RequestProperties, runContext: LambdaContext) async -> LambdaResult<Error> {
@@ -15,5 +23,15 @@ public struct Application {
 
 extension Application {
   public enum Error: Swift.Error {
+  }
+}
+
+extension AWSClient {
+  func shutdown(eventLoop: EventLoop) -> EventLoopFuture<Void> {
+    let promise = eventLoop.makePromise(of: Void.self)
+
+    promise.completeWithTask { try await self.shutdown() }
+
+    return promise.futureResult
   }
 }
