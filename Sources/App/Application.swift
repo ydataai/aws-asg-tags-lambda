cc9eaa6d93a2c7c48cfb5fe8a7512c82ecc5868b
Luís Portela Afonso <luis.portela@ydata.ai>
2022-06-22 09:56:30 +0100

    initialize hulk and smash from run

--- Sources/App/Application.swift
+++ Sources/App/Application.swift
@@ -1,28 +1,38 @@
 import AWSLambdaRuntime
 import Models
 import SotoCore
+import SotoAutoScaling
 import SotoEKS
 
 public struct Application {
   let context: LambdaInitializationContext
 
   let awsClient: AWSClient
+  let hulk: Hulk
 
   public init(context: LambdaInitializationContext) {
     self.context = context
 
     self.awsClient = AWSClient(httpClientProvider: .createNewWithEventLoopGroup(self.context.eventLoop))
-
     self.context.terminator.register(name: "\(type(of: AWSClient.self))", handler: self.awsClient.shutdown)
+
+    let eks = EKS(client: self.awsClient)
+    let eksClient = EKSClient(logger: self.context.logger, provider: eks)
+
+    let asg = AutoScaling(client: self.awsClient)
+    let asgClient = ASGClient(logger: self.context.logger, provider: asg)
+
+    self.hulk = Hulk(asgClient: asgClient, eksClient: eksClient)
   }
 
   public func run(properties: RequestProperties, runContext: LambdaContext) async -> LambdaResult<Error> {
-    return .success(())
-  }
-}
+    do {
+      try await hulk.smash(properties)
 
-extension Application {
-  public enum Error: Swift.Error {
+      return .success(())
+    } catch {
+      return .failure(error)
+    }
   }
 }
 
