e6fe9eec27f4a5f08f61151a36ad83049b069299
Luís Portela Afonso <luis.portela@ydata.ai>
2022-09-23 17:28:40 +0100

    feat: read variables from environment as command (#22)

--- Sources/Command/Environment.swift
+++ Sources/Command/Environment.swift
@@ -1,21 +1,49 @@
+import ArgumentParser
 import Foundation
 
 enum Environment {
-  static var decoder: JSONDecoder = JSONDecoder()
+  static var decoder = JSONDecoder()
 
-  static func get<Key: CodingKey>(_ key: Key) -> String? {
-    ProcessInfo.processInfo.environment[key.stringValue]
+  @inlinable
+  static func get(_ key: String) throws -> String {
+    guard let value = ProcessInfo.processInfo.environment[key] else {
+      throw Error.keyNotFound
+    }
+
+    return value
+  }
+
+  static func get<Key: CodingKey>(_ key: Key) throws -> String {
+    try get(key.stringValue)
+  }
+
+  @inlinable
+  static func get<D: Decodable>(
+    _ key: String,
+    _ type: D.Type = D.self,
+    decoder: JSONDecoder? = nil
+  ) throws -> D {
+    guard let data = try get(key).data(using: .utf8) else {
+      throw Error.cannotConvertToData
+    }
+
+    let decoder = decoder ?? Self.decoder
+
+    return try decoder.decode(D.self, from: data)
   }
 
   static func get<Key: CodingKey, D: Decodable>(
     _ key: Key,
     _ type: D.Type = D.self,
-    decoder: JSONDecoder = Self.decoder
-  ) throws -> D? {
-    guard let data = get(key)?.data(using: .utf8) else {
-      return nil
-    }
+    decoder: JSONDecoder? = nil
+  ) throws -> D {
+    try get(key.stringValue)
+  }
+}
 
-    return try decoder.decode(D.self, from: data)
+extension Environment {
+  enum Error: Swift.Error {
+    case keyNotFound
+    case cannotConvertToData
   }
 }
